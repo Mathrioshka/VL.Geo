d5b23afc6c10217e7bfeeb7ee68ab55accb4c604
Vadim Smakhtin <vadim.smakhtin@habidatum.com>
2021-08-10 10:54:30 +0300

    Initial commit

---
+++ src/PseudoMercator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VL.Geo.Projections
+{
+  public static class PseudoMercator
+  {
+    public static float YToLatitude(float y)
+    {
+      return (float)Math.Atan(
+        (float)Math.Exp(y / 180 * Math.PI)
+      ) / (float)Math.PI * 360 - 90;
+    }
+    public static float LatitudeToY(float latitude)
+    {
+      return (float)Math.Log(
+        (float)Math.Tan((latitude + 90) / 360 * (float)Math.PI)
+      ) / (float)Math.PI * 180;
+    }
+  }
+}
